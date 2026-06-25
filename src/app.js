b5590ce422d50cc1dc255acb315f99678401c8d2
rodrigorunner <rodrigoregisrun@gmail.com>
2026-06-25 18:45:15 -0300

    node_env added.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express')
 const app = express()
 
@@ -8,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(tripRoutes)
+
 app.use(errorHandler)
 
 module.exports = app 
